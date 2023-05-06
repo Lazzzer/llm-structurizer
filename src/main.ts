c64c37bd1290cef5fac4d2fc382de222d24d0501
Lazzzer <dev@lazarpavicevic.ch>
2023-05-06 20:50:00 +0200

    feat: setup config (env vars) and OpenAPI package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('LLM-Structurizer API')
+    .setDescription(
+      'LLM-Structurizer is an API that allows you to structure your data in a way that is easy to use and understand with the power of Large Language Models.',
+    )
+    .setVersion('1.0')
+    .addTag('llm-structurizer')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
