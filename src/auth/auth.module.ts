51283a66002907588920e3af1f942878a4e4b0eb
Lazzzer <dev@lazarpavicevic.ch>
2023-05-07 01:40:18 +0200

    feat: add entities for the db

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { ApiKeyStrategy } from './strategy/apiKey.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApiKey } from 'src/database/entities/api-key.entity';
+import { Application } from 'src/database/entities/application.entity';
 
 @Module({
-  imports: [PassportModule],
+  imports: [PassportModule, TypeOrmModule.forFeature([ApiKey, Application])],
   providers: [AuthService, ApiKeyStrategy],
   exports: [AuthService],
 })
--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,7 @@ async function bootstrap() {
         type: 'apiKey',
         name: 'X-API-KEY',
         in: 'header',
+        description: 'API Key for authentication of registered applications',
       },
       'apiKey',
     )
