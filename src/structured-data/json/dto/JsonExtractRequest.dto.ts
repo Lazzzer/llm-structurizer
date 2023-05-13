6ced3823f79dd3d53d881cf696f46e96317cf29a
Lazzzer <dev@lazarpavicevic.ch>
2023-05-14 01:56:07 +0200

    docs: add OpenAPI annotations

--- src/structured-data/json/dto/JsonExtractRequest.dto.ts
+++ src/structured-data/json/dto/JsonExtractRequest.dto.ts
@@ -1,18 +1,11 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsEnum, IsJSON, IsNotEmpty, IsNotEmptyObject } from 'class-validator';
+import { IsEnum, IsJSON, IsNotEmpty } from 'class-validator';
 
 enum Model {
   GPT_3_5_TURBO = 'gpt-3.5-turbo',
-  GPT_4 = 'gpt-4',
 }
 
 export class JsonExtractRequestDto {
-  @ApiProperty({
-    description: 'text to extract structured data from',
-  })
-  @IsNotEmpty()
-  text: string;
-
   @ApiProperty({
     enum: Model,
     description: 'model available for data extraction',
@@ -20,16 +13,21 @@ export class JsonExtractRequestDto {
   @IsEnum(Model)
   model: Model;
 
-  @ApiProperty({
-    description: 'json schema to use as model for data extraction',
-  })
-  @IsJSON()
-  @IsNotEmptyObject()
-  jsonSchema: string;
-
   @ApiPropertyOptional({
     description: 'whether to use refine multi-step extraction',
     default: false,
   })
   refine?: boolean;
+
+  @ApiProperty({
+    description: 'text to extract structured data from',
+  })
+  @IsNotEmpty()
+  text: string;
+
+  @ApiProperty({
+    description: 'json schema to use as model for data extraction',
+  })
+  @IsJSON()
+  jsonSchema: string;
 }
--- src/structured-data/json/json.controller.ts
+++ src/structured-data/json/json.controller.ts
@@ -1,15 +1,35 @@
 import {
   Body,
   Controller,
+  HttpCode,
   InternalServerErrorException,
   Post,
   UnprocessableEntityException,
 } from '@nestjs/common';
 import { JsonService } from './json.service';
-import { ApiSecurity, ApiTags } from '@nestjs/swagger';
-import { JsonExtractRequestDto } from './dto/JsonExtractRequest.dto';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiOkResponse,
+  ApiOperation,
+  ApiSecurity,
+  ApiTags,
+  ApiUnauthorizedResponse,
+  ApiUnprocessableEntityResponse,
+} from '@nestjs/swagger';
+import { JsonExtractRequestDto } from './dto/jsonExtractRequest.dto';
 import { InvalidJsonOutputError } from './exceptions/exceptions';
+import { JsonExtractResultDto } from './dto/jsonExtractResult.dto';
 
+@ApiUnauthorizedResponse({
+  description: "The API key in request's header is missing or invalid.",
+})
+@ApiBadRequestResponse({
+  description: 'The request body is invalid or missing.',
+})
+@ApiUnprocessableEntityResponse({
+  description: 'The output is not valid json.',
+})
 @ApiSecurity('apiKey')
 @ApiTags('structured-data')
 @Controller({
@@ -19,6 +39,25 @@ import { InvalidJsonOutputError } from './exceptions/exceptions';
 export class JsonController {
   constructor(private readonly jsonService: JsonService) {}
 
+  @ApiOperation({
+    summary: 'Return structured data from text as json using a json schema',
+    description: `This endpoint returns structured data from input text as json.  
+    It accepts a json schema as model for data extraction. The Refine technique can be used for longer texts.\n
+
+    Available model: gpt-3.5-turbo
+    `,
+  })
+  @ApiOkResponse({
+    type: JsonExtractResultDto,
+    description:
+      'The text was successfully structured as json. The output is a valid json object.',
+  })
+  @ApiBody({
+    type: JsonExtractRequestDto,
+    description:
+      'Request body containing text to process as json and extraction parameters',
+  })
+  @HttpCode(200)
   @Post('schema')
   async extractSchema(@Body() request: JsonExtractRequestDto) {
     const { text, model, jsonSchema, refine } = request;
@@ -31,7 +70,12 @@ export class JsonController {
         model,
         jsonSchema,
       );
-      return json;
+      const response: JsonExtractResultDto = {
+        model,
+        refine: refine || false,
+        output: JSON.stringify(json),
+      };
+      return response;
     } catch (e) {
       if (e instanceof InvalidJsonOutputError) {
         throw new UnprocessableEntityException(e.message);
